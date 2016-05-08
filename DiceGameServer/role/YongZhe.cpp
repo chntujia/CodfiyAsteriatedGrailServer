1d00fdd2bd4fa968b419efecbc5281802fcdfd0c
Gary Chan <chntujia@gmail.com>
2016-05-08 11:00:52 +0800

    Revert "修复勇者怒吼不中后，精疲力尽追刀会加蓝"
    This reverts commit 1939644396af4b11754576c2a181d5b701a1c7a4.

--- DiceGameServer/role/YongZhe.cpp
+++ DiceGameServer/role/YongZhe.cpp
@@ -1,189 +1,593 @@
-﻿#include "YongZhe.h"
+#include "YongZhe.h"
+#include "..\GameGrail.h"
+#include "..\UserTask.h"
 
-enum CAUSE{
-	TIAO_XIN = 2101,
-    JIN_DUAN_ZHI_LI = 2102,
-	NU_HOU = 2103,
-	MING_JING_ZHI_SHUI = 2104,
-	SI_DOU = 2105,
-	JING_PI_LI_JIE = 2106,
-};
-
-YongZhe::YongZhe()
+YongZhe::YongZhe(GameGrail *engine, int id, int color): PlayerEntity(engine, id, color)
 {
-    makeConnection();
-setMyRole(this);
+	this->roleID = 21;//设置Role ID 为21， 用于在GrailState.cpp进行挑衅的攻击目标判断
+	tokenMax[0]=4;//怒气 最大值均为4
+	tokenMax[1]=4;//知性
+	token[0] = token[1] = 0;
+	  //【被动】勇者之心  初始+2 水晶
+	setCrystal(2);
+	GameInfo game_info;
+	Coder::energyNotice(id, gem, crystal, game_info);
+	engine->sendMessage(-1, MSG_GAME, game_info);
 
-    Button *tiaoXin;
-    tiaoXin=new Button(3,QStringLiteral("挑衅"));
-    buttonArea->addButton(tiaoXin);
-    connect(tiaoXin,SIGNAL(buttonSelected(int)),this,SLOT(TiaoXin()));
+	tap = false;
+	using_NuHou = 0;
 }
 
-void YongZhe::normal()
-{
-    Role::normal();
-    Player* myself=dataInterface->getMyself();
-    //挑衅
-    if(myself->getToken(0)>0)
-        buttonArea->enable(3);
-        foreach(Player* ptr,dataInterface->getPlayerList())
-            if(ptr->getSpecial(1)==1){
-                buttonArea->disable(3);
-                break;
-            }
-    unactionalCheck();
+int YongZhe::RemoveTiaoXinEffect(int currentPlayer){
+	PlayerEntity* pe = engine->getPlayerEntity(currentPlayer);
+	if( GE_SUCCESS == pe->checkExclusiveEffect(EX_TIAO_XIN)){
+		pe->removeExclusiveEffect(EX_TIAO_XIN);
+		GameInfo update_info;	
+		Coder::exclusiveNotice(pe->getID(), pe->getExclusiveEffect(), update_info);
+		engine->sendMessage(-1, MSG_GAME, update_info);
+	}
+	return GE_SUCCESS;
 }
 
-void YongZhe::NuHou()
+bool YongZhe::cmdMsgParse(UserTask *session, uint16_t type, ::google::protobuf::Message *proto)
 {
-	gui->reset();
-    state=NU_HOU;
-    tipArea->setMsg(QStringLiteral("是否发动怒吼？"));
-    decisionArea->enable(0);
-    decisionArea->enable(1);
-}
+	switch(type)
+	{
+	case MSG_RESPOND:
+		Respond* respond = (Respond*)proto;
+		switch(respond->respond_id())
+		{
+			case  JIN_DUAN_ZHI_LI:
+				//tryNotify负责向游戏主线程传消息，只有id等于当前等待id，声明state等于当前state，声明step等于当前step，游戏主线程才会接受
+				//禁断之力的两种情形共用id
+				if(engine->topGameState()->state == STATE_TIMELINE_2_HIT){
+					session->tryNotify(engine->getCurrentPlayerID(), STATE_TIMELINE_2_HIT,  JIN_DUAN_ZHI_LI, respond);
+				}else{
+					session->tryNotify(engine->getCurrentPlayerID(), STATE_TIMELINE_2_MISS, JIN_DUAN_ZHI_LI, respond);
+				}
+				return true;
 
-void YongZhe::MingJingZhiShui()
-{
-	gui->reset();
-    state=MING_JING_ZHI_SHUI;
-    tipArea->setMsg(QStringLiteral("是否发动明镜止水？"));
-    decisionArea->enable(0);
-    decisionArea->enable(1);
-}
-
-void YongZhe::JinDuanZhiLi()
-{
-	gui->reset();
-    state=JIN_DUAN_ZHI_LI;
-    tipArea->setMsg(QStringLiteral("是否发动禁断之力？"));
-    decisionArea->enable(0);
-    decisionArea->enable(1);
-	
-}
-
-void YongZhe::SiDou()
-{
-	gui->reset();
-    state=SI_DOU;
-    tipArea->setMsg(QStringLiteral("是否发动死斗？"));
-    decisionArea->enable(0);
-    decisionArea->enable(1);
-}
-
-void YongZhe::TiaoXin()
-{
-    handArea->reset();
-    tipArea->reset();
-    playerArea->reset();
-    state=TIAO_XIN;
-
-    playerArea->enableEnemy();
-    playerArea->setQuota(1);
-
-    decisionArea->enable(1);
-    decisionArea->disable(0);
-}
-
-void YongZhe::onOkClicked()
-{
-    Role::onOkClicked();
-    SafeList<Player*>selectedPlayers;
-
-    selectedPlayers=playerArea->getSelectedPlayers();
-
-    network::Action* action;
-    network::Respond* respond;
-    try{
-    switch(state)
-    {
-
-    //挑衅
-	case TIAO_XIN:
-        action = newAction(ACTION_MAGIC_SKILL,TIAO_XIN);
-		action->set_src_id(myID);
-        action->add_dst_ids(selectedPlayers[0]->getID());
-        action->add_args(1);
-		usedMagic=true;
-        emit sendCommand(network::MSG_ACTION, action);
-        gui->reset();
-        break;
-    //禁断之力
-    case JIN_DUAN_ZHI_LI:
-        respond = newRespond(JIN_DUAN_ZHI_LI);
-        respond->add_args(1);
-		 
-        foreach(Card*ptr,dataInterface->getHandCards()){
-			respond->add_card_ids(ptr->getID());
+			case NU_HOU:
+				session->tryNotify(id, STATE_TIMELINE_1, NU_HOU, respond);
+				return true;
+			case SI_DOU:
+				session->tryNotify(id,STATE_TIMELINE_6_DRAWN,SI_DOU, respond);
+				return true;
+			case MING_JING_ZHI_SHUI:
+				session->tryNotify(id,STATE_TIMELINE_1,MING_JING_ZHI_SHUI, respond);
+				return true;
 		}
-        emit sendCommand(network::MSG_RESPOND, respond);
-        gui->reset();
-        break;
-	 //怒火，明镜止水，死斗
-    case NU_HOU:
-	case MING_JING_ZHI_SHUI:
-	case SI_DOU:
-        respond = newRespond(state);
-        respond->add_args(1);
-        emit sendCommand(network::MSG_RESPOND, respond);
-        gui->reset();
-        break;
-    }
-
-    }catch(int error){
-        logic->onError(error);
-    }
+	}
+	//没匹配则返回false
+	return false;
 }
 
-void YongZhe::onCancelClicked()
-{
-    Role::onCancelClicked();
-    QString command;
-	network::Action* action;
-    network::Respond* respond;
-    switch(state)
-    {
+//[响应]【怒吼】：（主动攻击前发动①，移除1点【怒气】）本次攻击伤害额外+2
+int YongZhe::NuHou(CONTEXT_TIMELINE_1 *con){
+	if (token[0] <=0){
+		if(token[0] <0){
+			return GE_FATAL_ERROR;
+		}
+		return GE_SUCCESS;
+	}
 
-    //禁断之力，怒吼，明镜止水，死斗
-	case TIAO_XIN:
-		normal();
-		break;
-    case JIN_DUAN_ZHI_LI:
-    case NU_HOU:
-	case MING_JING_ZHI_SHUI:
-	case SI_DOU:
-		respond = newRespond(state);
-        respond->add_args(0);
-        gui->reset();
-		emit sendCommand(network::MSG_RESPOND, respond);
-        break;
+	int ret = GE_SUCCESS;
+		//等待玩家响应
+	CommandRequest cmd_req;
+	Coder::askForSkill(id, NU_HOU, cmd_req);
+	if(engine->waitForOne(id, network::MSG_CMD_REQ, cmd_req))
+	{
+		void* reply;
+		if (GE_SUCCESS == (ret = engine->getReply(id, reply)))
+		{
+			Respond* respond = (Respond*) reply;
+			if(respond->args(0) == 1){
+				//确认发动，宣告技能
+				SkillMsg skill;
+				Coder::skillNotice(id, con->attack.dstID, NU_HOU, skill);
+				engine->sendMessage(-1, MSG_SKILL, skill);
 
-    }
+				//消耗1怒气
+				token[0]--;
+				GameInfo update_info;
+				Coder::tokenNotice(id, 0, token[0], update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+
+				//伤害+2
+				con->harm.point += 2;
+
+				//设置怒吼标记
+				using_NuHou = 1;
+			}
+		}
+		return ret;
+	}else{
+		return GE_TIMEOUT;
+	}
+
 }
-void YongZhe::turnBegin()
-{
-    Role::turnBegin();
+
+//[响应]【明镜止水】：（主动攻击前发动①，移除4点【知性】）本次攻击对方不能应战。
+int YongZhe::MingJingZhiShui(CONTEXT_TIMELINE_1 *con){
+	if (token[1] != 4){
+		if(token[1] <0){
+			return GE_FATAL_ERROR;
+		}
+		return GE_SUCCESS;
+	}
+
+	int ret = GE_SUCCESS;
+		//等待玩家响应
+	CommandRequest cmd_req;
+	Coder::askForSkill(id, MING_JING_ZHI_SHUI, cmd_req);
+	if(engine->waitForOne(id, network::MSG_CMD_REQ, cmd_req))
+	{
+		void* reply;
+		if (GE_SUCCESS == (ret = engine->getReply(id, reply)))
+		{
+			Respond* respond = (Respond*) reply;
+			if(respond->args(0) == 1){
+				//确认发动，宣告技能
+				SkillMsg skill;
+				Coder::skillNotice(id, con->attack.dstID, MING_JING_ZHI_SHUI, skill);
+				engine->sendMessage(-1, MSG_SKILL, skill);
+
+				//消耗4知性
+				token[1]= 0 ;
+				GameInfo update_info;
+				Coder::tokenNotice(id, 1, token[1], update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+
+				//不能应战
+				con->hitRate = RATE_NOREATTACK;
+			}
+		}
+		return ret;
+	}else{
+		return GE_TIMEOUT;
+	}
+
 }
 
-void YongZhe::askForSkill(network::Command* cmd)
+int YongZhe::p_timeline_1(int &step, CONTEXT_TIMELINE_1* con){
+	int ret = GE_INVALID_STEP;
+	if(con->attack.srcID != id || !con->attack.isActive){ //确认攻击是否由该id发动
+		return GE_SUCCESS;
+	}
+	if(step == STEP_INIT || step == NU_HOU){
+		step = NU_HOU;
+		ret = NuHou(con);
+		if(toNextStep(ret)){
+			step = MING_JING_ZHI_SHUI;
+		}
+	}
+	if(step == MING_JING_ZHI_SHUI){				
+		ret = MingJingZhiShui(con);
+		if(toNextStep(ret)){
+			step = STEP_DONE;
+		}
+	}
+	return ret;
+}
+
+//若未命中，你+1【知性】。
+int YongZhe::NuHouMiss(CONTEXT_TIMELINE_2_MISS *con){
+	if(using_NuHou !=0){
+		using_NuHou = 0;
+		
+		//+1【知性】
+		setToken(1,token[1]+1);
+		GameInfo update_info;
+		Coder::tokenNotice(id, 1, token[1], update_info);
+		engine->sendMessage(-1, MSG_GAME, update_info);
+	}
+	return GE_SUCCESS;
+}
+
+int YongZhe::p_timeline_2_hit(int &step, CONTEXT_TIMELINE_2_HIT * con)
 {
-    switch(cmd->respond_id())
-    {
-		case TIAO_XIN:
-			return TiaoXin();
-		case JIN_DUAN_ZHI_LI:
-			return JinDuanZhiLi();
-		case NU_HOU:
-			return NuHou();
-		case MING_JING_ZHI_SHUI:
-			return MingJingZhiShui();
-		case SI_DOU:
-			return SiDou();
+	if(con->attack.srcID != id || !con->attack.isActive){ //确认攻击是否由该id发动
+		return GE_SUCCESS;
+	}
+	step = JIN_DUAN_ZHI_LI;
+	int ret = JinDuanZhiLiHit(con);
+	if(toNextStep(ret) || ret == GE_URGENT){
+		step = STEP_DONE;
+	}
+	return ret;
+}
+
+int YongZhe::p_before_turn_begin(int &step, int currentPlayerID){
+	toRemoveTiaoXin = false;
+	step = STEP_DONE;
+	return GE_SUCCESS;
+}
+
+int YongZhe::p_before_action(int &step, int currentPlayerID) { 
+	/*
+		挑衅效果判断在 GrailState.cpp中完成，请使用[YongZhe]标记定位
+	*/
+	PlayerEntity* pe = engine->getPlayerEntity(currentPlayerID);
+	if( GE_SUCCESS == pe->checkExclusiveEffect(EX_TIAO_XIN)){
+		toRemoveTiaoXin = true;
+	}
 	
-		break;
-	default:
-        Role::askForSkill(cmd);
-    }
+	//解除精疲力竭
+	if(currentPlayerID == id){
+		if(tap){
+			//对自己造成3点法术伤害
+			HARM harm;
+			harm.cause = JING_PI_LI_JIE;
+			harm.point = 3;
+			harm.srcID = id;
+			harm.type = HARM_MAGIC;
+			engine->setStateTimeline3(id, harm);
+
+			GameInfo game_info;
+			tap = false;
+			Coder::tapNotice(id, false, game_info);
+			engine->sendMessage(-1, MSG_GAME, game_info);
+
+			this->setHandCardsMaxFixed(false);
+			engine->setStateChangeMaxHand(id, false, false,6 ,0);
+
+			return GE_URGENT;
+		}
+	}
+
+	return GE_SUCCESS;
 }
 
+int YongZhe::p_turn_end(int &step, int playerID)
+{
+	if(toRemoveTiaoXin){
+		int ret = RemoveTiaoXinEffect(playerID);
+		if(ret == GE_SUCCESS){
+			step = STEP_DONE;
+		}
+		return ret;
+	}
+	else 
+		return GE_SUCCESS;
+}
+
+int YongZhe::JingPiLiJie(){
+	tap = true;
+	GameInfo game_info;
+	Coder::tapNotice(id, true, game_info);
+
+	addAction(ACTION_ATTACK, JING_PI_LI_JIE);
+
+	this->setHandCardsMaxFixed(true,4);
+	Coder::handcardMaxNotice(id, this->getHandCardMax(), game_info);
+	engine->sendMessage(-1, MSG_GAME, game_info);
+	
+	return GE_SUCCESS;
+}
+
+int YongZhe::JinDuanZhiLiHit(CONTEXT_TIMELINE_2_HIT *con)//[响应]A 【禁断之力】：【能量】×1 （主动攻击命中或未命中后发动②）弃掉你所有手牌【展示】，其中每有1张法术牌，你+1点【怒气】；若命中②，其中每有1张火系牌，本次攻击伤害额外+1，并对自己造成等同于火系牌数量的法术伤害③
+{
+	int energy = getEnergy();
+	if(energy<=0){
+		return GE_SUCCESS;	
+	}
+	int ret = GE_SUCCESS;
+
+		//等待玩家响应
+	CommandRequest cmd_req;
+	Coder::askForSkill(id, JIN_DUAN_ZHI_LI, cmd_req);
+	if(engine->waitForOne(id, network::MSG_CMD_REQ, cmd_req))
+	{
+		void* reply;
+		if (GE_SUCCESS == (ret = engine->getReply(id, reply)))
+		{
+			
+			Respond* respond = (Respond*) reply;
+			if(respond->args(0) == 1){
+				//确认发动，宣告技能
+				SkillMsg skill;
+				Coder::skillNotice(id, con->attack.dstID, JIN_DUAN_ZHI_LI, skill);
+				engine->sendMessage(-1, MSG_SKILL, skill);
+
+				//消耗1能量
+				//此处优先扣除水晶
+				GameInfo update_info;
+				if(crystal>0){
+					setCrystal(--crystal);
+				}
+				else{
+					setGem(--gem);
+				}
+				Coder::energyNotice(id, gem, crystal, update_info);
+
+				//计算特殊牌数目
+				vector<int> cards;
+				int card_id = 0;
+				int magicCardNum = 0;
+				int fireCardNum = 0;
+				for (int i = 0; i < respond->card_ids_size(); ++i)
+				{
+					card_id = respond->card_ids(i);
+					CardEntity * ce = getCardByID(card_id);
+					if(ce->getType() == TYPE_MAGIC){
+						magicCardNum ++;
+					}
+					if(ce->getElement() == ELEMENT_FIRE){
+						fireCardNum ++;
+					}
+					cards.push_back(card_id);
+				}
+
+				//每有1张法术牌，你+1点【怒气】
+				setToken(0,token[0]+magicCardNum);
+
+				Coder::tokenNotice(id, 0, token[0], update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+
+				//每有1张火系牌，本次攻击伤害额外+1
+				con->harm.point += fireCardNum;
+				//发动精疲力竭
+				JingPiLiJie();
+				//对自己造成等同于火系牌数量的法术伤害③
+				if(fireCardNum>0){
+					HARM harm;
+					harm.cause = JIN_DUAN_ZHI_LI;
+					harm.point = fireCardNum;
+					harm.srcID = id;
+					harm.type = HARM_MAGIC;
+					engine->setStateTimeline3(id, harm);
+				}
+				//展示并丢弃手牌
+				if (cards.size() > 0)
+				{
+					CardMsg show_card;
+					Coder::showCardNotice(id, cards.size(), cards, show_card);
+					engine->sendMessage(-1, MSG_CARD, show_card);
+					engine->setStateMoveCardsNotToHand(id, DECK_HAND, -1, DECK_DISCARD, cards.size(), cards, id, JIN_DUAN_ZHI_LI, true);
+					
+				}
+				
+				return GE_URGENT;
+
+			}
+		}
+		return ret;
+	}else{
+		return GE_TIMEOUT;
+	}
+}
+
+int YongZhe::JinDuanZhiLiMiss(CONTEXT_TIMELINE_2_MISS *con)//[响应]A 【禁断之力】：【能量】×1 （主动攻击命中或未命中后发动②）弃掉你所有手牌【展示】，其中每有1张法术牌，你+1点【怒气】；若未命中②，其中每有1张水系牌，你+1点【知性】；
+{
+	int energy = getEnergy();
+	if(energy<=0){
+		if(energy < 0){
+			return GE_FATAL_ERROR;
+		}
+		return GE_SUCCESS;
+	
+	}
+	int ret = GE_SUCCESS;
+
+			//等待玩家响应
+	CommandRequest cmd_req;
+	Coder::askForSkill(id, JIN_DUAN_ZHI_LI, cmd_req);
+	if(engine->waitForOne(id, network::MSG_CMD_REQ, cmd_req))
+	{
+		void* reply;
+		if (GE_SUCCESS == (ret = engine->getReply(id, reply)))
+		{
+			Respond* respond = (Respond*) reply;
+			if(respond->args(0) == 1){
+				//确认发动，宣告技能
+				SkillMsg skill;
+				Coder::skillNotice(id, con->dstID, JIN_DUAN_ZHI_LI, skill);
+				engine->sendMessage(-1, MSG_SKILL, skill);
+
+				//消耗1能量
+				//此处优先扣除水晶
+				GameInfo update_info;
+				if(crystal>0){
+					setCrystal(--crystal);
+				}
+				else{
+					setGem(--gem);
+				}
+				Coder::energyNotice(id, gem, crystal, update_info);
+
+				//计算特殊牌数目
+				vector<int> cards;
+				int card_id = 0;
+				int magicCardNum = 0;
+				int waterCardNum = 0;
+				for (int i = 0; i < respond->card_ids_size(); ++i)
+				{
+					card_id = respond->card_ids(i);
+					CardEntity * ce = getCardByID(card_id);
+					if(ce->getType() == TYPE_MAGIC){
+						magicCardNum ++;
+					}
+					if(ce->getElement() == ELEMENT_WATER){
+						waterCardNum ++;
+					}
+					cards.push_back(card_id);
+				}
+
+				//每有1张法术牌，你+1点【怒气】
+				setToken(0,token[0]+magicCardNum);
+
+				Coder::tokenNotice(id, 0, token[0], update_info);
+
+				//每有1张水系牌，你+1点【知性】
+				setToken(1,token[1]+waterCardNum);
+
+				Coder::tokenNotice(id, 1, token[1], update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+
+				//发动精疲力竭  , 插入了状态 因此必须返回urgent
+				JingPiLiJie();
+				//展示并丢弃手牌
+				if (cards.size() > 0)
+				{
+					CardMsg show_card;
+					Coder::showCardNotice(id, cards.size(), cards, show_card);
+					engine->sendMessage(-1, MSG_CARD, show_card);
+					engine->setStateMoveCardsNotToHand(id, DECK_HAND, -1, DECK_DISCARD, cards.size(), cards, id, JIN_DUAN_ZHI_LI, true);					
+				}
+				return GE_URGENT;
+			}
+		}
+	}
+	return GE_SUCCESS;
+}
+
+int YongZhe::p_timeline_2_miss(int &step, CONTEXT_TIMELINE_2_MISS * con)
+{
+	int ret = GE_INVALID_STEP;
+	if(con->srcID != id || !con->isActive){ //确认攻击是否由该id发动
+		return GE_SUCCESS;
+	}
+	if(step == STEP_INIT || step == NU_HOU){
+		step = NU_HOU;
+		ret = NuHouMiss(con);
+		step = JIN_DUAN_ZHI_LI;
+	}
+	if(step == JIN_DUAN_ZHI_LI){
+		ret = JinDuanZhiLiMiss(con);
+		if(toNextStep(ret) || ret == GE_URGENT){
+			step = STEP_DONE;
+		}
+	}
+
+	return ret;
+}
+
+int YongZhe::SiDou(CONTEXT_TIMELINE_6_DRAWN *con){
+
+	//消耗1个宝石
+	if(gem <= 0){
+		return GE_SUCCESS;
+	}
+	int ret = GE_SUCCESS;
+		//等待玩家响应
+	CommandRequest cmd_req;
+	Coder::askForSkill(id, SI_DOU, cmd_req);
+	if(engine->waitForOne(id, network::MSG_CMD_REQ, cmd_req))
+	{
+		void* reply;
+		if (GE_SUCCESS == (ret = engine->getReply(id, reply)))
+		{
+			Respond* respond = (Respond*) reply;
+			if(respond->args(0) == 1){
+				//确认发动，宣告技能
+				SkillMsg skill;
+				Coder::skillNotice(id, con->dstID, SI_DOU, skill);
+				engine->sendMessage(-1, MSG_SKILL, skill);
+
+				//消耗宝石
+				setGem(--gem);
+				GameInfo update_info;
+				Coder::energyNotice(id, gem, crystal, update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+
+				//怒气+3
+				setToken(0, (token[0] + 3));
+				Coder::tokenNotice(id, 0, token[0], update_info);
+				engine->sendMessage(-1, MSG_GAME, update_info);
+			}
+		}
+		return ret;
+	}else{
+		return GE_TIMEOUT;
+	}
+
+}
+int YongZhe::p_timeline_6_drawn(int &step, CONTEXT_TIMELINE_6_DRAWN *con){
+	if(con->dstID == id){
+		if(con->harm.point > 0 && con->harm.type == HARM_MAGIC){
+			if(gem<=0){return GE_SUCCESS;}
+			step = SI_DOU;
+			int ret = SiDou(con);
+			if(toNextStep(ret)){
+				step = STEP_DONE;
+			}
+			return ret;
+		}
+	}
+	return GE_SUCCESS;
+	
+}
+int YongZhe::TiaoXin(Action* action)//[法术]【挑衅】：（移除1点【怒气】，将【挑衅】放置于目标对手面前）你+1【知性】；该对手在其下个行动阶段必须且只能主动攻击你，否则他跳过该行动阶段，触发后移除此牌。
+{
+	int dstID = action->dst_ids(0);
+
+	//怒气-1
+	token[0]--;
+	GameInfo update_info;
+	Coder::tokenNotice(id, 0, token[0], update_info);
+	engine->sendMessage(-1, MSG_GAME, update_info);
+
+	//知性+1
+	setToken(1,token[1]+1);
+	Coder::tokenNotice(id, 1, token[1], update_info);
+	engine->sendMessage(-1, MSG_GAME, update_info);
+
+	//更新专属
+	PlayerEntity* dst = engine->getPlayerEntity(dstID);
+	dst->addExclusiveEffect(EX_TIAO_XIN);
+	Coder::exclusiveNotice(dstID, dst->getExclusiveEffect(), update_info);
+	engine->sendMessage(-1, MSG_GAME, update_info);
+	
+	//确认发动，宣告技能
+	SkillMsg skill;
+	Coder::skillNotice(id, dstID, TIAO_XIN, skill);
+	engine->sendMessage(-1, MSG_SKILL, skill);
+				
+	return GE_SUCCESS;
+}
+
+
+
+//P_MAGIC_SKILL只有发起者执行，故不需要判断发起者，法术技能验证均在v_MAGIC_SKILL中
+int YongZhe::p_magic_skill(int &step, Action *action)
+{
+	int ret;
+	int actionID = action->action_id();
+	switch(actionID)
+	{
+		case TIAO_XIN:
+			ret = TiaoXin(action);
+			step = STEP_DONE;
+			
+			break;
+		
+		default:
+			return GE_INVALID_ACTION;
+	}
+	return ret;
+}
+//客户端按钮是否允许发动的检测由客户端完成。
+int YongZhe::v_magic_skill(Action *action)
+{
+	int actionID = action->action_id();
+	int playerID = action->src_id();
+	int cardID = 0;
+	CardEntity * card = NULL ;
+	if(playerID != id){
+		return GE_INVALID_PLAYERID;
+	}
+	switch(actionID)
+	{
+		case TIAO_XIN:
+			if(token[0]>0){ //怒气>0
+				return GE_SUCCESS;
+			}
+			
+			break;
+	
+		
+		default:
+			return GE_INVALID_ACTION;
+	}
+	return  GE_INVALID_ACTION;
+}
--- DiceGameServer/role/YongZhe.h
+++ DiceGameServer/role/YongZhe.h
@@ -1,25 +1,36 @@
-﻿#ifndef YONGZHE_H
-#define YONGZHE_H
-#include "Role.h"
-class YongZhe : public Role
+#pragma once
+#include "..\PlayerEntity.h"
+
+class YongZhe : public PlayerEntity
 {
-    Q_OBJECT
 public:
-    YongZhe();
-    void normal();
-    void NuHou();
-    void MingJingZhiShui();
-    void JinDuanZhiLi();
-    void SiDou();
-    void turnBegin();
-    void askForSkill(network::Command* cmd);
-    void additionalAction();
-public slots:
-    void onOkClicked();
-    void onCancelClicked();
-    void TiaoXin();
+	YongZhe(GameGrail *engine, int id, int color);
+	int p_timeline_1(int &step, CONTEXT_TIMELINE_1* con);
+	int p_timeline_2_miss(int &step, CONTEXT_TIMELINE_2_MISS *con);
+	int p_timeline_2_hit(int &step, CONTEXT_TIMELINE_2_HIT * con);
+	int p_timeline_6_drawn(int &step, CONTEXT_TIMELINE_6_DRAWN *con);
+	int p_magic_skill(int &step, Action* action);
+	int v_magic_skill(Action *action);
+	bool cmdMsgParse(UserTask* session, uint16_t type, ::google::protobuf::Message *proto);
+	int p_before_turn_begin(int &step, int currentPlayerID);
+	int p_before_action(int &step, int currentPlayerID);
+	int p_turn_end(int &step, int playerID);
 private:
+	//具体技能
+	int TiaoXin(Action* action);//[法术]【挑衅】：（移除1点【怒气】，将【挑衅】放置于目标对手面前）你+1【知性】；该对手在其下个行动阶段必须且只能主动攻击你，否则他跳过该行动阶段，触发后移除此牌。
+	int RemoveTiaoXinEffect(int currentPlayer);
+	int NuHou(CONTEXT_TIMELINE_1 *con);//[响应]【怒吼】：（主动攻击前发动①，移除1点【怒气】）本次攻击伤害额外+2；若未命中，你+1【知性】。
+	int NuHouMiss(CONTEXT_TIMELINE_2_MISS *con);
+	int using_NuHou;//是否在怒吼效果中
+	int MingJingZhiShui(CONTEXT_TIMELINE_1 *con);//[响应]【明镜止水】：（主动攻击前发动①，移除4点【知性】）本次攻击对方不能应战。
+	
+	int JinDuanZhiLiHit(CONTEXT_TIMELINE_2_HIT *con);//[响应]A 【禁断之力】：【能量】×1 （主动攻击命中或未命中后发动②）弃掉你所有手牌【展示】，其中每有1张法术牌，你+1点【怒气】；若未命中②，其中每有1张水系牌，你+1点【知性】；若命中②，其中每有1张火系牌，本次攻击伤害额外+1，并对自己造成等同于火系牌数量的法术伤害③
+	int JinDuanZhiLiMiss(CONTEXT_TIMELINE_2_MISS *con);
+
+	int JingPiLiJie();//[被动]A 【精疲力竭】：（发动【禁断之力】后强制触发【强制】）【横置】额外+1【攻击行动】；持续到你的下个行动阶段开始，你的手牌上限恒定为4【恒定】。【精疲力竭】的效果结束时【重置】，并对自己造成3点法术伤害③。
+	int SiDou(CONTEXT_TIMELINE_6_DRAWN *con);//[响应]D 【死斗】：【宝石】×1 （每当你承受法术伤害时发动⑥）你+3点【怒气】。 　*【怒气】和【知性】为勇者专有指示物，上限各为4。
+	bool toRemoveTiaoXin;//挑衅前触发了虚弱跳过
+	 
+	
+	
 };
-
-
-#endif // YONGZHE_H
